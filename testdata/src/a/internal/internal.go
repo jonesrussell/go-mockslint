de1de60091c51fa0dca02bca12837564e6cf7b89
Russell Jones <499552+jonesrussell@users.noreply.github.com>
2025-01-27 23:58:05 -0500

    refactor: remove fx-related code and focus on mock location rules

--- testdata/src/a/internal/internal.go
+++
@@ -1,9 +0,0 @@
-package internal
-
-// InternalService is a service in the internal package
-type InternalService struct{}
-
-// NewInternalService creates a new InternalService
-func NewInternalService() *InternalService {
-	return &InternalService{}
-}
--- testdata/src/a/test/mocks/mock_service.go
+++
@@ -1,3 +0,0 @@
-package mocks
-
-type MockService struct{}
